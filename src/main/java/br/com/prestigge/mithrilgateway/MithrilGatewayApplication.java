0f3d66ad50a3ab0fa55a62db5ce8bed867a4120b
Ricardo Borzani <ricardo.borzani@yahoo.com>
2023-04-26 17:11:23 -0300

    The structure of the project was modified, using the spring cloud gateway reference project as template
    Signed-off-by: Ricardo Borzani <ricardo.borzani@yahoo.com>

--- src/main/java/br/com/prestigge/mithrilgateway/MithrilGatewayApplication.java
+++ src/main/java/br/com/prestigge/mithrilgateway/MithrilGatewayApplication.java
@@ -1,29 +1,60 @@
 package br.com.prestigge.mithrilgateway;
 
+import reactor.core.publisher.Mono;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.gateway.config.GatewayAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableDiscoveryClient
-@EnableEurekaClient
-@SpringBootApplication(exclude = GatewayAutoConfiguration.class)
-@ComponentScan(basePackages = "br.com.prestigge.mithrilgateway")
-public class MithrilGatewayApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MithrilGatewayApplication.class);
-	}
+@SpringBootApplication
+@EnableConfigurationProperties(UriConfiguration.class)
+@RestController
+public class MithrilGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MithrilGatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+		String httpUri = uriConfiguration.getHttpbin();
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri(httpUri))
+				.route(p -> p
+						.host("*.circuitbreaker.com")
+						.filters(f -> f
+								.circuitBreaker(config -> config
+										.setName("mycmd")
+										.setFallbackUri("forward:/fallback")))
+						.uri(httpUri))
+				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
+}
+
+@ConfigurationProperties
+class UriConfiguration {
+
+	private String httpbin = "http://httpbin.org:80";
+
+	public String getHttpbin() {
+		return httpbin;
+	}
+
+	public void setHttpbin(String httpbin) {
+		this.httpbin = httpbin;
+	}
 }
