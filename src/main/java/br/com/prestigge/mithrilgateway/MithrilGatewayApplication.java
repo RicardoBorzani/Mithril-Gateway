c7078aa6f86e0d8c7a9e130eddbf4b365e825f35
Ricardo Borzani <ricardo.borzani@yahoo.com>
2023-04-26 02:04:57 -0300

    Not working version

--- src/main/java/br/com/prestigge/mithrilgateway/MithrilGatewayApplication.java
+++ src/main/java/br/com/prestigge/mithrilgateway/MithrilGatewayApplication.java
@@ -2,9 +2,25 @@ package br.com.prestigge.mithrilgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.gateway.config.GatewayAutoConfiguration;
 
-@SpringBootApplication
-public class MithrilGatewayApplication {
+@Configuration
+@EnableDiscoveryClient
+@EnableEurekaClient
+@SpringBootApplication(exclude = GatewayAutoConfiguration.class)
+@ComponentScan(basePackages = "br.com.prestigge.mithrilgateway")
+public class MithrilGatewayApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MithrilGatewayApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MithrilGatewayApplication.class, args);
--- src/main/java/br/com/prestigge/mithrilgateway/config/GatewayConfig.java
+++ src/main/java/br/com/prestigge/mithrilgateway/config/GatewayConfig.java
@@ -1,13 +1,15 @@
 package br.com.prestigge.mithrilgateway.config;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @Configuration
+@EnableDiscoveryClient
 public class GatewayConfig implements ApplicationListener<WebServerInitializedEvent> {
 
     @Override
@@ -17,18 +19,8 @@ public class GatewayConfig implements ApplicationListener<WebServerInitializedEv
     }
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("auth", r -> r.path("/auth/**").uri("http://localhost:8080/auth-service"))
-                .route("user", r -> r.path("/users/**").uri("http://localhost:8080/user-service"))
-                .route("config", r -> r.path("/config/**").uri("http://localhost:8080/config-service"))
-                .route("transactions", r -> r.path("/transactions/**").uri("http://transactions-service:8080"))
-                .route("calculations", r -> r.path("/calculations/**").uri("http://calculations-service:8080"))
-                .route("reports", r -> r.path("/reports/**").uri("http://reports-service:8080"))
-                .route("import", r -> r.path("/import/**").uri("http://import-service:8080"))
-                .route("logs", r -> r.path("/api/logs/**").uri("http://logs-service:8080"))
-                .route("finance", r -> r.path("/finance/**").uri("http://finance-service:8080"))
-                .build();
+    public RouteLocatorBuilder routeLocatorBuilder() {
+        return new RouteLocatorBuilder(null);
     }
 
 }
