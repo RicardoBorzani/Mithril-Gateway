430465dfb1848b1d4f38c4067616b8f0db003b6e
Ricardo Borzani <ricardo.borzani@yahoo.com>
2023-04-28 09:31:14 -0300

    Attempt to implement the circuitbreaker architecture showing problems in the Resilience4j and Hystrix libraries, only the Resilience4j library will be implemented.
    Signed-off-by: Ricardo Borzani <ricardo.borzani@yahoo.com>

--- src/main/java/br/com/prestigge/mithrilgateway/MithrilGatewayApplication.java
+++ src/main/java/br/com/prestigge/mithrilgateway/MithrilGatewayApplication.java
@@ -1,16 +1,29 @@
 package br.com.prestigge.mithrilgateway;
 
-import reactor.core.publisher.Mono;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
+import java.time.Duration;
 
 import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.runApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableConfigurationProperties(UriConfiguration.class)
@@ -24,17 +37,40 @@ public class MithrilGatewayApplication {
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
 		String httpUri = uriConfiguration.getHttpbin();
+
+		CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+				.failureRateThreshold(50)
+				.waitDurationInOpenState(Duration.ofMillis(10000))
+				.permittedNumberOfCallsInHalfOpenState(5)
+				.slidingWindowSize(10)
+				.minimumNumberOfCalls(5)
+				.build();
+
 		return builder.routes()
 				.route(p -> p
-						.path("/get")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri(httpUri))
-				.route(p -> p
-						.host("*.circuitbreaker.com")
+						.path("/auth/**")
 						.filters(f -> f
 								.circuitBreaker(config -> config
-										.setName("mycmd")
-										.setFallbackUri("forward:/fallback")))
+										.setName("authCircuitBreaker")
+										.setFallbackUri("forward:/authFallback")
+										//.setCircuitBreakerConfig(circuitBreakerConfig)
+								)
+								.retry(config -> config
+										.setRetries(3)
+										//.setStatuses(HttpStatus.BAD_GATEWAY)
+								)
+								//.hystrix(config -> config
+										//.setName("authHystrixCommand")
+										//.setFallbackUri("forward:/authFallback")
+								//)
+								//.requestRateLimiter(config -> config
+										//.setKeyResolver(new SpELClientKeyResolver())
+										//.setRateLimiter(new RedisRateLimiter(10, 20))
+								//)
+								//.timeout(config -> config
+										//.setResponseTimeout(Duration.ofMillis(2000))
+								//		)
+						)
 						.uri(httpUri))
 				.build();
 	}
@@ -44,6 +80,8 @@ public class MithrilGatewayApplication {
 		return Mono.just("fallback");
 	}
 }
+
+
 
 @ConfigurationProperties
 class UriConfiguration {
