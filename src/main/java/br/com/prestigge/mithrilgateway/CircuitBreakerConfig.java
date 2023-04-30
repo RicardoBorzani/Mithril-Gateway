bc1edd27a520acb39c6042487599568fee88c56a
Ricardo Borzani <ricardo.borzani@yahoo.com>
2023-04-30 16:46:09 -0300

    Create CircuitBreakerConfig class
    Signed-off-by: Ricardo Borzani <ricardo.borzani@yahoo.com>

---
+++ src/main/java/br/com/prestigge/mithrilgateway/CircuitBreakerConfig.java
@@ -0,0 +1,42 @@
+package br.com.prestigge.mithrilgateway;
+
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CircuitBreakerConfig {
+
+    @Bean
+    public TimeLimiterConfig timeLimiterConfig() {
+        return TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofMillis(500))
+                .build();
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> customizer() {
+
+        return factory -> factory.configure(builder -> builder
+                .timeLimiterConfig(timeLimiterConfig())
+                .circuitBreakerConfig(circuitBreakerConfiguration())
+                .build(), "backendService");
+    }
+
+    private io.github.resilience4j.circuitbreaker.CircuitBreakerConfig circuitBreakerConfiguration() {
+        return io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .slowCallRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(1000))
+                .slowCallDurationThreshold(Duration.ofSeconds(2))
+                .permittedNumberOfCallsInHalfOpenState(3)
+                .minimumNumberOfCalls(10)
+                .slidingWindowType(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
+                .slidingWindowSize(5)
+                .build();
+    }
+}
