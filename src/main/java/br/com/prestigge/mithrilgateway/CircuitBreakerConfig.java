8c5a899b22383053e50441f82e3f4af845648817
Ricardo Borzani <ricardo.borzani@yahoo.com>
2023-04-30 21:02:23 -0300

    Try to upgrade CircuitBreakerConfig

--- src/main/java/br/com/prestigge/mithrilgateway/CircuitBreakerConfig.java
+++ src/main/java/br/com/prestigge/mithrilgateway/CircuitBreakerConfig.java
@@ -2,15 +2,19 @@ package br.com.prestigge.mithrilgateway;
 
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+
 
 import java.time.Duration;
 
 @Configuration
 public class CircuitBreakerConfig {
-
+    /*
     @Bean
     public TimeLimiterConfig timeLimiterConfig() {
         return TimeLimiterConfig.custom()
@@ -38,5 +42,19 @@ public class CircuitBreakerConfig {
                 .slidingWindowType(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
                 .slidingWindowSize(5)
                 .build();
-    }
+    }*/
+
+    @Bean
+    public CircuitBreakerFactory circuitBreakerFactory() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .slowCallRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(1000))
+                .slowCallDurationThreshold(Duration.ofSeconds(2))
+                .permittedNumberOfCallsInHalfOpenState(3)
+                .minimumNumberOfCalls(10)
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
+                .slidingWindowSize(5)
+                .build();
+        return new Resilience4JCircuitBreakerFactory(config);
 }
